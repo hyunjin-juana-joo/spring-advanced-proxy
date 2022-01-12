0c6dc4ba3faf759a9cafb5b8d8b09785ea95d5e8
주현진/전시개발팀 <juana.joo@musinsa.com>
2022-01-12 23:25:09 +0900

    스프링 핵심 원리 | 빈 후처리기

--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -3,8 +3,10 @@ package hello.proxy.app.v1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")
